c67429b4105198c9d05fb4c795883299e31c9164
Kush Patel <kushpatel2354@gmail.com>
2025-10-09 16:02:58 +0530

    Polymorphism

---
+++ com.kush.java/polimorphism/CompileTimePolymorphism.java
@@ -0,0 +1,27 @@
+package polimorphism;
+
+import static statickeyword.staticImport.kp;
+
+public class CompileTimePolymorphism {
+    public static void main(String[] args) {
+        int intResult = new CompileTimePolymorphism().add(5, 10);
+        double doubleResult = new CompileTimePolymorphism().add(5.5, 10.5);
+        String stringResult = new CompileTimePolymorphism().add("Hello, ", "World!");
+        kp(intResult);
+        kp(doubleResult);
+        kp(stringResult);
+    }
+    int add(int a, int b) {
+        return a + b;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+    String add(String a, String b) {
+        return a + b;
+    }
+}
+// Output:
+// 15
+// 16.0
+// Hello, World!
---
+++ com.kush.java/polimorphism/RunTimePolymorphism.java
@@ -0,0 +1,23 @@
+package polimorphism;
+
+import inheritance.Animal;
+import inheritance.Cat;
+import inheritance.Dog;
+
+public class RunTimePolymorphism {
+    public static void main(String[] args) {
+        Animal a; // Declare a variable of type Animal
+        a = new Dog("Buddy", "Brown"); // Assign a Dog object to the Animal variable
+        a.makeSound(); // Call the sound method, which will execute Dog's version
+
+        a = new Cat("Whiskers", "White"); // Assign a Cat object to the same Animal variable
+        a.makeSound(); // Call the sound method, which will execute Cat's version
+    }
+}
+// Output:
+//Animal constructor called
+//Dog constructor called
+//Buddy says: Woof! Woof!
+//Animal constructor called
+//Cat constructor called
+//Whiskers says: Meow! Meow!
